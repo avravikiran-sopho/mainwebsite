782aca5e36ce7e45186d6bc3aabe8016cfa24246
avravikiran-sopho <me15btech11039@iith.ac.in.com>
2017-12-12 20:56:06 +0530

    added beta

--- elan_main/settings.py
+++ elan_main/settings.py
@@ -147,5 +147,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-ALLOWED_HOSTS = [u'139.59.71.180',u'127.0.0.1']
+ALLOWED_HOSTS = [u'139.59.71.180',u'127.0.0.1',u'elan.org.in']
 
