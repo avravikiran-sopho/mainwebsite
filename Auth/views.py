b7c752c1cb292787c38327c7990a58e74f5946e7
avravikiran-sopho <me15btech11039@iith.ac.in.com>
2017-12-17 08:57:07 +0530

    to server

--- Auth/views.py
+++ Auth/views.py
@@ -27,7 +27,7 @@ def Login(request):
 	if request.method == "POST":
 		form = LoginForm(request.POST)
 		if form.is_valid():
-			username = ['username']
+			username = form.cleaned_data['username']
 			password = form.cleaned_data['password']
 			user = authenticate(username=username,password=password)
 			if user is not None:
