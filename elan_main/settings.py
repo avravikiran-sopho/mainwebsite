7a30a3eeedeafffc6fbf868d65aa162df6f59e8b
avravikiran-sopho <me15btech11039@iith.ac.in.com>
2017-12-18 18:23:09 +0530

    to server email

--- elan_main/settings.py
+++ elan_main/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [ STATIC_PATH, ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = '74.125.22.108'
 EMAIL_HOST_USER = 'noreply@elan.org.in'
 EMAIL_HOST_PASSWORD = 'iith@2018'
 EMAIL_PORT = 587
