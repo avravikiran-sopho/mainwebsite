3b7bf044cb1b76d60d9bd9335953191f2dbb1e71
avravikiran-sopho <me15btech11039@iith.ac.in.com>
2018-01-05 02:36:38 +0530

    admin

--- Auth/admin.py
+++ Auth/admin.py
@@ -3,4 +3,9 @@ from .models import Profile
 
 
 # Register your models here.
-admin.site.register(Profile)
+
+class DisplayProfile(admin.ModelAdmin):
+	list_display = ('user','full_name','college','mobile','country','elanids')
+	search_fields = ['elanids','mobile','full_name','country']
+	ordering = ('user','elanids')
+admin.site.register(Profile, DisplayProfile)
