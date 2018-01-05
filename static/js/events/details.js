743a3d26a6abd9d9e9ba2532e11ed50730c7f9f2
avravikiran-sopho <me15btech11039@iith.ac.in.com>
2018-01-05 10:57:39 +0530

    biggies

--- static/js/events/details.js
+++ static/js/events/details.js
@@ -2081,6 +2081,7 @@ $('.card').on('click',function () {
         Travel on a double bass and riff through the jungle which has creatures who are calm like a bomb. Elan is back with its signature Event, the metal battle. Get ready to put the stage on Wildfire.<br>\
         'For those about to rock, we salute you!'' - AC/DC<br>\
         <h1>Event Format</h1>\ <p>This event will consist of 2 rounds</p>\  <h1>Prelims:</h1>\ <p>Each band interested in participating should upload their demos/videos to <a   href='https://drive.google.com/drive/folders/1CfX1U7KmDeJQPNSaJeQ7n6xPvQy2Dh6K?usp=sharing'>This Drive</a>\
+        <br>and fill out this <a href=''https://goo.gl/forms/7gxAkYFkM0z9bPZk2>form</a><br>(Do not upload more than 2 videos in the drive link.)</p>\ 
         Out of this 5 bands will be shortlisted, and will be performing at Elan-e-Jung prelims, Elan 2018.<br>\
         Each band will be given 20 minutes to perform including sound check.</p>\
         <h1>Finals:</h1>\
@@ -2100,7 +2101,7 @@ $('.card').on('click',function () {
         9867327421</p>\
         ";
     var manthan = "<h1 style='text-align:center'>Manthan</h1>\
-        <h1>Event Format</h1>\ <p>This event will consist of 2 rounds</p>\  <h1>Prelims:</h1>\ <p>Each band interested in participating should upload their videos on <a href='https://drive.google.com/drive/folders/1iXDE-BvcyYqf4840BDVmcJTj-wCiVDsZ?usp=sharing'>This Drive</a><br>\  Out of this 5 bands will be shortlisted, and will be performing at Manthan prelims, Elan 2017.<br>\
+        <h1>Event Format</h1>\ <p>This event will consist of 2 rounds</p>\  <h1>Prelims:</h1>\ <p>Each band interested in participating should upload their videos on <a href='https://drive.google.com/drive/folders/1iXDE-BvcyYqf4840BDVmcJTj-wCiVDsZ?usp=sharing'>This Drive</a><br>and fill this <a href='https://goo.gl/forms/1c3QzWwPk0Pxj5OS2'>form</a><br>upload more than 2 videos in the drive link.<br>\  Out of this 5 bands will be shortlisted, and will be performing at Manthan prelims, Elan 2017.<br>\
         Each band will be given 20 minutes to perform including sound check.<br>\
         <h1>RULES AND REGULATIONS</h1>\
         <p>Any non-professional band is allowed to register. You need not be a college band to participate in this event.<br>\
