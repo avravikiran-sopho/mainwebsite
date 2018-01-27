c5e4869fd056f583055b6fdc4bcd5e881d599408
avravikiran-sopho <me15btech11039@iith.ac.in>
2018-01-27 20:31:24 +0530

    litw

--- webapp/urls.py
+++ webapp/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     url(r'^mypledge$', views.social),
 	url(r'^deregister$', views.deregister),
 	url(r'^litfest$', views.litfest),
-	url(r'^litw$', views.litw),
+	url(r'^litw$', views.spokenword),
     # url(r'^mypledge_message$', views.message),
 ]
